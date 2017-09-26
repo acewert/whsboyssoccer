203cce86400403814a33a92892d810b8957d7453
Andrew Ewert <andrew@vestmed.com>
2017-09-26 15:42:05 -0500

    Latest

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+__all__ = ['Player']
+
+
+class Player(models.Model):
+    class classes:
+        FRESHMAN = 1
+        SOPHOMORE = 2
+        JUNIOR = 3
+        SENIOR = 4
+
+    class squads:
+        FRESHMAN = 1
+        JV = 2
+        VARSITY = 3
+
+    class positions:
+        FORWARD = 1
+        MIDFIELDER = 2
+        DEFENDER = 3
+        GOALKEEPER = 4
+
+    CLASS_CHOICES = (
+        (classes.FRESHMAN, 'Freshman'),
+        (classes.SOPHOMORE, 'Sophomore'),
+        (classes.JUNIOR, 'Junior'),
+        (classes.SENIOR, 'Senior'),
+    )
+
+    SQUAD_CHOICES = (
+        (squads.FRESHMAN, 'Freshman'),
+        (squads.JV, 'JV'),
+        (squads.VARSITY, 'Varsity'),
+    )
+
+    POSITION_CHOICES = (
+        (positions.FORWARD, 'Forward'),
+        (positions.MIDFIELDER, 'Midfielder'),
+        (positions.DEFENDER, 'Defender'),
+        (positions.GOALKEEPER, 'Goalkeeper'),
+    )
+
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    number = models.CharField(max_length=3)
+    klass = models.IntegerField(choices=CLASS_CHOICES)
+    squad = models.IntegerField(choices=SQUAD_CHOICES)
+    position = models.IntegerField(choices=POSITION_CHOICES)
+
+    def __str__(self):
+        return '{0} {1} (#{2})'.format(self.first_name, self.last_name,
+                                       self.number)
--- app/views.py
+++ app/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
 
+from .models import *
+
 
 def home(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
 
 def roster(request):
-	return render(request, 'roster.html')
+    freshman = Player.objects.filter(squad=Player.squads.FRESHMAN)
+    jv = Player.objects.filter(squad=Player.squads.JV)
+    varsity = Player.objects.filter(squad=Player.squads.VARSITY)
+
+    context = {
+        'freshman': freshman,
+        'jv': jv,
+        'varsity': varsity,
+    }
+
+    return render(request, 'roster.html', context)
 
 
 def schedule(request):
-	return render(request, 'schedule.html')
+    return render(request, 'schedule.html')
 
 
 def coaches(request):
-	return render(request, 'coaches.html')
+    return render(request, 'coaches.html')
 
 
 def history(request):
-	return render(request, 'history.html')
+    return render(request, 'history.html')
 
 
 def photos(request):
-	return render(request, 'photos.html')
+    return render(request, 'photos.html')
 
 
 def sponsor(request):
-	return render(request, 'sponsor.html')
+    return render(request, 'sponsor.html')
